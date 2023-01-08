c9111b681338a9375fae662212ec7641d168b430
Ramadevi-89 <122200073+Ramadevi-89@users.noreply.github.com>
2023-01-08 13:46:58 +0530

    Add files via upload
    WEEK1 DAY1

---
+++ selenium/Mobile.java
@@ -0,0 +1,15 @@
+package selenium;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		int cost = 15000;
+		Boolean QualityIsGood = true;
+		String model = "Realme note 7 pro";
+		 System.out.println("Price of the Mobile = " +" " +cost +" "+ "Model of the mobile = " +model);
+		System.out.println("Quality of the mobile = "+ QualityIsGood);
+		
+		
+	}
+
+}
---
+++ selenium/Week1_day1.java
@@ -0,0 +1,10 @@
+package selenium;
+
+public class Week1_day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my first class");
+	}
+
+}
